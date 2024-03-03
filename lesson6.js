586d15379f2017ae90c26b96647b2472357e577b
eugen <evgenheller666@gmail.com>
2024-03-03 23:19:00 +0400

    ASTONJS-1 [feart]:hw6 done

---
+++ lesson6.js
@@ -0,0 +1,20 @@
+//Промисы
+//1) 1-3-6-4-2-5
+//2) 1-3-2
+//3) abc
+//4) 1 123 2 123 3 321 4 undefined
+//5) 1 4 3 2
+
+//delayFunc
+let devArr = [5,1,6,4,7]
+const delayFunc = (arr) => {
+    if (arr.every(Number.isInteger) && arr.length !== 0) {
+        arr.forEach((el, i) => {
+            setTimeout(() => console.log(i), (i + 1) * 3000)
+        })
+    }else{
+        return console.log("Массив состоит не из целых чисел")
+    }
+}
+console.log(delayFunc(devArr))
+
